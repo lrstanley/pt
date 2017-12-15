3a25de443cf0370349a99dcdcad5898440f60cbc
Liam Stanley <me@liamstanley.io>
2017-12-14 19:53:11 -0500

    add json filter

---
+++ filters.go
@@ -0,0 +1,37 @@
+// Copyright (c) Liam Stanley <me@liamstanley.io>. All rights reserved. Use
+// of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+package pt
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+
+	"github.com/flosch/pongo2"
+)
+
+func init() {
+	pongo2.RegisterFilter("json", filterJSON)
+}
+
+func filterJSON(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	var b bytes.Buffer
+	enc := json.NewEncoder(&b)
+
+	// This doesn't need to be done, as pongo2 by default will escape vars.
+	// enc.SetEscapeHTML(true)
+
+	if args := strings.ToLower(param.String()); args == "pretty" {
+		enc.SetIndent("", "    ")
+	} else if args != "" {
+		enc.SetIndent("", args)
+	}
+
+	if err := enc.Encode(in.Interface()); err != nil {
+		return nil, &pongo2.Error{Sender: "filter:json", OrigError: err}
+	}
+
+	return pongo2.AsValue(b.String()), nil
+}
